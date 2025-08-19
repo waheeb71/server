975b4d524c9f57df8f6b10c0d04bdb308fd126da
waheeb mahyoob <134747118+waheeb71@users.noreply.github.com>
2025-08-19 19:31:34 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,31 +2,19 @@ from flask import Flask, request, send_file
 from weasyprint import HTML
 import io
 from flask_cors import CORS
-import os
+
 app = Flask(__name__)
 CORS(app)  # للسماح بالطلبات من React
 
-# ضع هنا Base64 للعلامة المائية
-
-WATERMARK_PATH = os.path.join(app.root_path,'watermark.png')
 @app.route('/generate-pdf', methods=['POST'])
 def generate_pdf():
     try:
         data = request.json
         html_content = data.get('html', '<h1>Empty</h1>')
 
-        # إضافة العلامة المائية
-        html_with_watermark = f"""
-        <div style="position: relative;">
-            <img src="{WATERMARK_PATH}" 
-                 style="position: absolute; top: 30%; left: 25%; width: 50%; opacity: 0.15; z-index: -1;" />
-            {html_content}
-        </div>
-        """
-
         # تحويل HTML إلى PDF بحجم A4
         pdf_file = io.BytesIO()
-        HTML(string=html_with_watermark).write_pdf(pdf_file)
+        HTML(string=html_content).write_pdf(pdf_file)
         pdf_file.seek(0)
 
         return send_file(
